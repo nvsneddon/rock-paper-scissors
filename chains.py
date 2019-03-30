5c3850187f2c2c15b0eb1ac9f0d38ef0bc677a78
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2019-03-30 13:59:50 -0600

    Working markov chain making

--- chains.py
+++ chains.py
@@ -16,11 +16,27 @@ class Chains:
             except FileNotFoundError:
                 predictions = self.createlayer()
         self.__chain = predictions
-        print(type(self.__chain))
-        print(self.__chain)
+
+    def __updateChain(self, chain=dict(), level=0, start=True):
+        if start:
+            return self.__updateChain(self.__chain, start=False)
+            
+
+        if level >= self.__chain_depth or level >= len(self.__pastmoves):
+            raise ValueError("The level is out of bounds.")
+
+        if not chain:
+            chain = self.createlayer()
+
+        if level == self.__chain_depth - 1 or level == len(self.__pastmoves)-1:
+            chain[self.__pastmoves[level].name]["freq"] += 1
+            return chain
+
+        chain[self.__pastmoves[level].name]["next"] = self.__updateChain(chain[self.__pastmoves[level].name]['next'], level=level+1, start=False)
+        return chain
+
 
     def savefile(self):
-        print(self.__filename)
         f = open(self.__filename, 'w')
         f.write(json.dumps(self.__chain))
         f.close()
@@ -28,10 +44,10 @@ class Chains:
     def makemove(self, move):
             if isinstance(move, str):
                 move = movesenum[move.lower()]
-            
             self.__pastmoves.append(move)
             if len(self.__pastmoves) > self.__chain_depth:
                 self.__pastmoves.pop(0)
+            self.__chain = self.__updateChain()
 
     def createlayer(self):
         predictions = dict()
@@ -44,8 +60,14 @@ class Chains:
                 predictions[i] = newdict
         return predictions
 
+    def getChain(self):
+        return self.__chain
+
 def main():
     somechain = Chains("Nathaniel")
+    somechain.makemove("rock")
+    somechain.makemove("paper")
+    somechain.makemove("scissors")
     somechain.savefile()
 
 if __name__ == "__main__":
