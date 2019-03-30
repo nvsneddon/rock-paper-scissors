6007cc901d8a42b9694bf8e4bc063447f0c94e19
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2019-03-30 12:22:23 -0600

    Made a working chain class

--- chains.py
+++ chains.py
@@ -1,25 +1,48 @@
 import json
+from rpsenum import movesenum
 
 class Chains:
     def __init__(self, name=""):
-        self.__chains = list()
+        self.__pastmoves = list()
+        self.__chain_depth = 5
+        self.__chain = dict()
+        self.__filename = name.lower()+'.json' 
+
         if name != "" and name != "allpeople":
             try:
-                f = open(name.lower() + '.json')
+                f = open(self.__filename)
                 predictions = json.loads(f.read())
                 f.close()
-                self.__chains.append(predictions)
             except FileNotFoundError:
-                self.__chains.append(createlayer())
-        self.__chains.append()
+                predictions = self.createlayer()
+        self.__chain = predictions
+        print(type(self.__chain))
+        print(self.__chain)
 
-def createlayer():
-    predictions = dict()
-    moves = ("rock", "paper", "scissors")
-    for i in moves:
-        if i not in predictions:
-            newdict = dict()
-            newdict["freq"] = 0
-            newdict["next"] = dict()
-            predictions[i] = newdict
-    return predictions
+    def savefile(self):
+        print(self.__filename)
+        f = open(self.__filename, 'w')
+        f.write(json.dumps(self.__chain))
+        f.close()
+
+    def makemove(self, move):
+            if isinstance(move, str):
+                move = movesenum[move.lower()]
+            self.__pastmoves.append(move)
+            if len(self.__pastmoves) > self.__chain_depth:
+                self.__pastmoves.pop(0)
+
+    def createlayer(self):
+        predictions = dict()
+        moves = ("rock", "paper", "scissors")
+        for i in moves:
+            if i not in predictions:
+                newdict = dict()
+                newdict["freq"] = 0
+                newdict["next"] = dict()
+                predictions[i] = newdict
+        return predictions
+
+
+somechain = Chains("Nathaniel")
+somechain.savefile()
