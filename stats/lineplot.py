eff02703dda10a0890fe824060f0c86b07852212
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2019-04-12 14:49:45 -0600

    Finished doing stats stuff

--- stats/lineplot.py
+++ stats/lineplot.py
@@ -1,10 +1,23 @@
 import matplotlib.pyplot as plt
+import json
+import sys
 
-x = [3,7,16]
-y = [1,4,7]
+if len(sys.argv) < 2:
+    print("Please specify a file")
+    exit()
 
+try:
+    f = open(sys.argv[1])
+    stats = json.loads(f.read())
+    f.close()
+except FileNotFoundError:
+    print("File not found")
+    exit()
+
+y = stats["avg_winning_rate"]
+x = stats["avg_winning_labels"]
 plt.plot(x, y, color='green')
-plt.xlabel("The X label")
-plt.ylabel("The Y label")
+plt.xlabel("Number of times played")
+plt.ylabel("Winning average of the human")
 plt.title("My title")
 plt.show()
--- stats/statstest.py
+++ stats/statstest.py
@@ -12,5 +12,11 @@ try:
 except FileNotFoundError:
     print("File not found")
     exit()
-print(spstats.ttest_1samp(losses, 0.33))
-print(stats.stdev(losses))
+tstat, pvalue = spstats.ttest_1samp(losses, 0.33)
+
+print("The computer has lost", sum(losses), "games")
+print("The computer has played", len(losses), "games")
+print("The average of the losses is {:1.4}".format(sum(losses)/len(losses)))
+print("The Std dev is {:1.4}".format(stats.stdev(losses)))
+
+print("The stats test gives a T value of {:2.6} and a pvalue of {:4.4}".format(tstat, pvalue))
